e1dffbb32587d717d9c38b295f021608d5cbf70e
Michael Quinn <mquinn5@binghamton.edu>
2020-02-25 16:47:13 -0500

    correcting solution

--- hw5.pl
+++ hw5.pl
@@ -168,9 +168,6 @@ d( U - V, x, RU - RV ):-d(U,x,RU), d(V,x,RV).
 d(U * V,x, U * DV + V * DU):- d(U,x,DU), d(V,x,DV).
 d(U ^ N, x, N*U ^ N1*DU) :- integer(N), N1 is N-1, d(U, x, DU).
 
-evaluate(A,B,_):-
-	B is A,
-	number(B).
 evaluate(A X B, An N Bn,[X:N|T]):-
 	atom(X),
 	number(N),
@@ -178,7 +175,9 @@ evaluate(A X B, An N Bn,[X:N|T]):-
 	evaluate(B, Bn, [X:N|T]).
 evaluate(A, S, [X:N|T]):-
 	evaluate(A, S, T).
-
+evaluate(A,B,[]):-
+	B is A,
+	number(B).
 
 /* Problem 4 Tests:  */
 :- evaluate(x*y, 6, [x:2, y:3]).
